1c338fd050d2a2f7566e9bb069662d6bcfc2eb34
axaysushir <sinewave_11@yahoo.com>
2020-05-07 18:29:56 +0530

    add text info to eartquake

---
+++ earthdata/env.js
@@ -0,0 +1,4 @@
+var env = require('dotenv')
+env.config()
+
+DB_CONNECTION = process.env.DB_CONNECTION
--- earthdata/sketch.js
+++ earthdata/sketch.js
@@ -12,17 +12,19 @@ var earthquakes;
 function preload() {
   // The clon and clat in this url are edited to be in the correct order.
   mapimg = loadImage(
-    "https://api.mapbox.com/styles/v1/mapbox/dark-v9/static/0,0,1,0/1024x512?access_token=YOUR_ACCESS_TOKEN"
-   
+    "https://api.mapbox.com/styles/v1/mapbox/dark-v9/static/0,0,1,0/1024x512?access_token=pk.eyJ1IjoiYWtzaGF5c3VzaGlyIiwiYSI6ImNrOTQ4dzdvejAyeDczZXNsYnZlMTI0ZWMifQ.6cgRLMiTiAWkuVNEumaCzA"
   );
-//   earthquakes = loadStrings(
-//     "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.csv"
-//   );
-    earthquakes = loadStrings(
-      "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.csv"
-    );
-    //last 7 days
-    // earthquakes = loadStrings("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.csv")
+  //   earthquakes = loadStrings(
+  //     "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.csv"
+  //   );
+  // earthquakes = loadStrings(
+  //   "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.csv"
+  // );
+  //last 7 days
+  // earthquakes = loadStrings("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.csv")
+  earthquakes = loadStrings(
+    './earthquakes1970-2014.csv'
+  )
 }
 
 function mercX(lon) {
@@ -54,19 +56,19 @@ function setup() {
     //   console.log(data);
     var lat = data[1];
     var lon = data[2];
-    var mag = data[4]
+    var mag = data[4];
     var x = mercX(lon) - cx;
     var y = mercY(lat) - cy;
 
     // mag = pow(10, mag);
     // mag = sqrt(mag);
 
-    mag = pow( 10, mag / 2 );
+    mag = pow(10, mag / 2);
 
-    var magmax = sqrt(pow(10, 10))
+    var magmax = sqrt(pow(10, 10));
 
-    var d = map(mag, 0, magmax, 0, 180)
-    stroke(255, 0, 255) 
+    var d = map(mag, 0, magmax, 0, 180);
+    stroke(255, 0, 255);
 
     fill(255, 0, 0, 200);
     ellipse(x, y, d, d);
